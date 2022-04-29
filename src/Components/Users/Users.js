9cc271f0f305e4ab79df3d7f641ac536c35ba24f
Ayon <ayonjodder177@gmail.com>
2022-04-29 09:25:20 +0600

    Data send to the server side with fetch post

---
+++ src/Components/Users/Users.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Users = () => {
+    const handleSubmit = event => {
+        event.preventDefault()
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = { name, email };
+
+        fetch('http://localhost:5000/user', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => {
+                console.log('Error', error);
+            })
+    }
+    return (
+        <div>
+            <h1>Please add a new user</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder='name' id="" /><br/>
+                <input type="email" name="email" placeholder='email' id="" /><br/>
+                <input type="submit" value="Add User" id="" />
+            </form>
+        </div>
+    );
+};
+
+export default Users;
