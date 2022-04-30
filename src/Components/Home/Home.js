6f71f48988982cc36ee2b6cce05c747fa08fe7c1
Ayon <ayonjodder177@gmail.com>
2022-04-30 09:41:29 +0600

    Delete user done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Users from '../Users/Users';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -8,11 +7,28 @@ const Home = () => {
             .then(res => res.json())
             .then(data => setUsers(data))
     }, [])
+    const handleRemoveUser = id => {
+        const conformation = window.confirm('Do you want to delete?')
+        if (conformation) {
+            const url = `http://localhost:5000/user/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    //Updating UI after deleting
+                    if (data.deletedCount) {
+                        const remaining = users.filter(user => user._id !== id);
+                        setUsers(remaining);
+                    }
+                })
+        }
+    }
     return (
         <div>
 
             {
-                users.map(user => <li key={user._id}>{user.name}::{user.email}</li>)
+                users.map(user => <li key={user._id}>{user.name}::{user.email} <button onClick={() => handleRemoveUser(user._id)}>x</button></li>)
             }
         </div>
     );
